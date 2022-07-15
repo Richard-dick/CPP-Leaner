11e88a4e166b9ff59609075cf6741fdb8e35d6b1
dick <aoijf627@gmail.com>
2022-07-15 18:35:24 +0800

    15 prob in C2 are over

---
+++ Primer/Chapter-2/ex2-15.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int &rval = 1.01;
+    int &rval13;
+    return 0;
+}
---
+++ Primer/Chapter-2/ex2-8.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "2M\n" ;
+    
+    cout << "revised :"<<endl;
+
+    cout << "2\tM\n";
+    return 0;
+}
